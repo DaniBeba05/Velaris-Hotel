275ecd57c598543c487f74e9e95ca0dd29b4416c
usuario <usuario@DESKTOP-3213JP1>
2024-10-19 10:16:26 +0200

    Implementación de distintos ENUMS para las clases Cliente, Habitación, Reserva Y Staff.  Closes #9.

---
+++ Velaris Hotel/src/velaris/hotel/EstadoHabitacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package velaris.hotel;
+
+/**
+ *
+ * @author elvid
+ */
+public enum EstadoHabitacion {
+    
+    DISPONIBLE,
+    NODISPONIBLE, 
+    LIMPIA, 
+    SUCIA;
+    
+}
