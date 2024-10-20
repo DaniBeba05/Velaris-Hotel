8719457913ff05acb95ad46f2ace73e1f4311c9a
usuario <usuario@DESKTOP-3213JP1>
2024-10-20 07:36:42 +0200

    Implementación de la clase Reserva y la clase Staff y un test de que funcionan correctamente. Closes #5, Closes #6, Closes #7, Closes #8.

--- Velaris Hotel/src/velaris/hotel/VelarisHotel.java
+++ Velaris Hotel/src/velaris/hotel/VelarisHotel.java
@@ -3,140 +3,103 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package velaris.hotel;
-import java.time.LocalDate;
+ import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 /**
  *
  * @author usuario
  */
-public class VelarisHotel {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-         
-        Habitación habitacion1 = new Habitación(101, "Suite", true, 200.00, true);
 
-      
-        System.out.println("Detalles de la habitacion 1:");
-        System.out.println(habitacion1.obtenerDescripcion());
 
+public class VelarisHotel{
+   
+   public static void main(String[] args) {
        
-        System.out.println("\nIntentando reservar habitacion 1...");
-        if (habitacion1.reservar()) {
-            System.out.println("Habitacion reservada con exito.");
-        } else {
-            System.out.println("La habitacion no esta disponible para reserva.");
-        }
-        
-        System.out.println(habitacion1.obtenerDescripcion());
-
-       
-        System.out.println("\nLiberando habitacion 1...");
-        habitacion1.liberar();
-        System.out.println(habitacion1.obtenerDescripcion());
-
-       
-        System.out.println("\nAplicando un descuento del 10% a la habitacion 1...");
-        habitacion1.aplicarDescuento(10);
-        System.out.println(habitacion1.obtenerDescripcion());
-
-       
-        Habitación habitacion2 = new Habitación(habitacion1);
-        System.out.println("\nDetalles de la habitacion 2 (copia de la 1):");
-        System.out.println(habitacion2.obtenerDescripcion());
-
-        
-        habitacion2.setNumeroHabitacion(102);
-        habitacion2.setTipoHabitacion("Individual");
-        habitacion2.aplicarCargoAdicional(50);
-        System.out.println("\nDetalles de la habitacion 1 (original) despues de modificar la copia:");
-        System.out.println(habitacion1.obtenerDescripcion());
-        System.out.println("\nDetalles de la habitacion 2 (modificada):");
-        System.out.println(habitacion2.obtenerDescripcion());
-
-       
-        System.out.println("\nEstableciendo precio de temporada alta en habitacion 2...");
-        habitacion2.establecerPrecioTemporada("alta");
-        System.out.println(habitacion2.obtenerDescripcion());
-
-        
-        System.out.println("\nComparando precios entre habitacion 1 y habitacion 2...");
-        if (habitacion1.compararPrecio(habitacion2)) {
-            System.out.println("La habitacion 1 es mas barata que la habitacion 2.");
-        } else {
-            System.out.println("La habitacion 1 no es mas barata que la habitacion 2.");
-        }
-
-        
-        double costoConImpuesto = habitacion1.calcularCostoConImpuesto(3, 10);
-        System.out.println("\nEl costo total para 3 noches en habitacion 1 con 10% de impuesto es: " + costoConImpuesto);
-
-       
-        System.out.println("\nReseteando la habitacion 1...");
-        habitacion1.resetear();
-        System.out.println(habitacion1.obtenerDescripcion());
+  
+    Cliente cliente1 = new Cliente("Carlos", "123ABC", 612345678, GeneroCliente.MASCULINO, LocalDate.of(1985, 5, 15));
+    Cliente cliente2 = new Cliente("Maria", "456DEF", 612345679, GeneroCliente.FEMENINO, LocalDate.of(1990, 8, 22));
     
-        Cliente cliente1 = new Cliente("Carlos", "123ABC", 612345678, Cliente.Genero.MASCULINO, LocalDate.of(1985, 5, 15));
-        System.out.println("Cliente 1:");
-        System.out.println(cliente1.obtenerInformacionDetallada());
-        System.out.println();
-
-       
-        Cliente cliente2 = new Cliente();
-        System.out.println("Cliente 2 (constructor por defecto):");
-        System.out.println(cliente2.obtenerInformacionDetallada());
-        System.out.println();
-
-        
-        Cliente cliente3 = new Cliente(cliente1);
-        System.out.println("Cliente 3 (constructor de copia de Cliente 1):");
-        System.out.println(cliente3.obtenerInformacionDetallada());
-        System.out.println();
-
-        
-        System.out.println("Validar numero de contacto de Cliente 1: " + cliente1.validarNumeroContacto());
-        System.out.println("Cliente 1 es mayor de edad: " + cliente1.esMayorDeEdad());
-        System.out.println("Cliente 1 es un numero movil: " + cliente1.esNumeroMovil());
-        System.out.println("Cliente 1 tiene ID valido: " + cliente1.validarID());
-        System.out.println();
-
-      
-        cliente2.actualizarCliente("Ana", "456DEF", 634567890, Cliente.Genero.FEMENINO);
-        cliente2.cambiarFechaNacimiento(LocalDate.of(2005, 8, 20));
-        System.out.println("Cliente 2 despues de actualizar:");
-        System.out.println(cliente2.obtenerInformacionDetallada());
-        System.out.println();
-
-       
-        Cliente cliente4 = cliente2.clonar();
-        System.out.println("Cliente 4 (clon de Cliente 2):");
-        System.out.println(cliente4.obtenerInformacionDetallada());
-        System.out.println();
-
-        
-        int comparacion = cliente1.compararEdad(cliente2);
-        if (comparacion > 0) {
-            System.out.println("Cliente 1 es mayor que Cliente 2.");
-        } else if (comparacion < 0) {
-            System.out.println("Cliente 1 es menor que Cliente 2.");
-        } else {
-            System.out.println("Cliente 1 y Cliente 2 tienen la misma edad.");
-        }
-        System.out.println();
-
-        
-        boolean cambioGenero = cliente1.cambiarGenero(Cliente.Genero.OTRO);
-        System.out.println("Cliente 1 despues de cambiar genero a OTRO:");
-        System.out.println("Cambio de genero: " + (cambioGenero ? "exitoso" : "fallido"));
-        System.out.println(cliente1.obtenerInformacionDetallada());
-        System.out.println();
-
-       
-        System.out.println("Categoria de edad de Cliente 2: " + cliente2.categoriaEdad());
-        System.out.println("Categoria de edad de Cliente 1: " + cliente1.categoriaEdad());
+  
+    LocalDate fechaEntrada1 = LocalDate.now().plusDays(4); 
+    LocalDate fechaSalida1 = fechaEntrada1.plusDays(3);
+    double costoTotal1 = 300.00; 
+    
+    Reserva reserva1 = new Reserva(fechaEntrada1, fechaSalida1, cliente1, EstadoReserva.PENDIENTE, true, costoTotal1);
+    
+    System.out.println(reserva1.mostrarDetalles());
+    reserva1.pagarReserva(); 
+    reserva1.confirmarReserva();
+    System.out.println(reserva1.mostrarDetalles());
+    
+    System.out.println("\nEl cliente " + cliente1.getNombre() + " decide cancelar la reserva...");
+    reserva1.cancelarReserva();
+    System.out.println(reserva1.mostrarDetalles());
+    
+    LocalDate fechaEntrada2 = LocalDate.now().plusDays(2);
+    LocalDate fechaSalida2 = fechaEntrada2.plusDays(2); 
+    double costoTotal2 = 200.00; 
+    
+    Reserva reserva2 = new Reserva(fechaEntrada2, fechaSalida2, cliente2, EstadoReserva.PENDIENTE, true, costoTotal2);
+    
+    System.out.println("\nDetalles de la segunda reserva:");
+    System.out.println(reserva2.mostrarDetalles());
+    
+    reserva2.pagarReserva();
+    reserva2.confirmarReserva();
+    System.out.println(reserva2.mostrarDetalles());
+    
+    System.out.println("\nEl cliente " + cliente2.getNombre() + " decide cancelar la reserva...");
+    reserva2.cancelarReserva();
+    System.out.println(reserva2.mostrarDetalles());
+    
+    // ----------------------------------------
+    //          Creación de un Staff
+    // ----------------------------------------
+    
+    Staff staff1 = new Staff("Juan Perez", 101, TipoStaff.RECEPCIONISTA, 3);
+    
+   
+    System.out.println("\nDetalles del Staff:");
+    System.out.println(staff1.obtenerResumen());
+    
+   
+    if (staff1.addReserva(reserva1)) {
+        System.out.println("\nReserva aniadida al Staff " + staff1.getnombre());
+    } else {
+        System.out.println("\nNo se pudo aniadir la reserva al Staff " + staff1.getnombre() + " porque ha alcanzado el limite.");
+    }
+    
+   
+    if (staff1.addReserva(reserva2)) {
+        System.out.println("\nSegunda reserva aniadida al Staff " + staff1.getnombre());
+    } else {
+        System.out.println("\nNo se pudo añadir la segunda reserva al Staff " + staff1.getnombre() + " porque ha alcanzado el limite.");
+    }
+    
+    
+    System.out.println("\nReservas actuales del Staff:");
+    for (Reserva reserva : staff1.getReservas()) {
+        System.out.println(reserva.mostrarDetalles());
+    }
+    
+    
+    if (staff1.maxReservasAlcanzado()) {
+        System.out.println("\nEl Staff " + staff1.getnombre() + " ha alcanzado el numero maximo de reservas permitidas.");
+    } else {
+        System.out.println("\nEl Staff " + staff1.getnombre() + " aun puede gestionar más reservas.");
     }
 }
+
+}
+
+
+
+
+
+  
+
     
 
