dbc96abab281b9b5808651f1b99a210113d44994
usuario <usuario@DESKTOP-3213JP1>
2024-10-31 16:57:57 +0100

    Creación de varios java package y correción de errores.

---
+++ Velaris Hotel/src/Enums/EstadoReserva.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Enums;
+
+/**
+ *
+ * @author usuario
+ */
+public enum EstadoReserva {
+    
+   CONFIRMADA, 
+   CANCELADA,
+   PENDIENTE;
+   
+}
---
+++ Velaris Hotel/src/Enums/TipoStaff.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Enums;
+
+/**
+ *
+ * @author usuario
+ */
+public enum TipoStaff {
+    
+   RECEPCIONISTA, 
+   COCINERO, 
+   LIMPIADOR, 
+   DIRECTOR, 
+   GERENTE,
+   CONSERJE,
+   PERSONALMANTENIMIENTO, 
+   SOCORRISTA;   
+}
